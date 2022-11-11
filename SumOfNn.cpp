eb34749dfad66c1477f9bf126e280a44d0878165
pratapshingane18 <sushrutrao465@gmail.com>
2022-11-11 13:43:44 +0530

    adding sum of n natural no prog

---
+++ SumOfNn.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Sum(int n){
+    if(n > 0){
+        return Sum(n-1) +n;
+    }
+    return 0;
+}
+
+int main(){
+  int a=10;
+  cout<<Sum(a)<<endl;
+
+  return 0;
+}
